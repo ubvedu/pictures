2796ff2b6843d317d0853a5cc656ee09f6cd7807
Roman Ukhobotov <ukhobotov.rv@phystech.edu>
2021-09-25 11:54:17 +0300

    Updated turtle1/stars task

--- turtle1/stars.py
+++ turtle1/stars.py
@@ -1,31 +1,40 @@
 from math import cos, pi
 import turtle
 
+
 radius = 100
-
-
-def draw_star(tl, n):
-    coms = [
-        lambda: tl.shape('turtle'),
-        lambda: tl.right(90 / n),
-    ]
-    for _ in range(n):
-        coms.append(lambda: tl.forward(2 * radius * cos(pi / 2 / n)))
-        coms.append(lambda: tl.left(180 - 180 / n))
-    return coms
 
 
 tl1 = turtle.Turtle(shape='turtle')
 tl1.penup()
 tl1.backward(2 * radius)
 tl1.pendown()
+tl2 = turtle.Turtle(shape='turtle')
 
-stars = [
-    draw_star(tl1, 5),
-    draw_star(turtle.Turtle(shape='turtle'), 11),
+
+drawers = [
+    [tl1, 5, 0],
+    [tl2, 11, 0],
 ]
 
-for i in range(max(map(len, stars))):
-    for star in stars:
-        if i < len(star):
-            star[i]()
+
+def prepare(tl, n):
+    tl.right(90 / n)
+
+
+def move(tl, n):
+    tl.forward(2 * radius * cos(pi / 2 / n))
+    tl.left(180 - 180 / n)
+
+
+for drawer in drawers:
+    prepare(drawer[0], drawer[1])
+
+done = False
+while not done:
+    done = True
+    for drawer in drawers:
+        if drawer[2] != drawer[1]:
+            move(drawer[0], drawer[1])
+            drawer[2] += 1
+            done = False
