7f2eff9a738da0c32e0771c9a29d98ad054c297e
Roman Ukhobotov <ukhobotov.rv@phystech.edu>
2021-10-01 17:58:22 +0300

    Drawn the lama

--- pictures1/hills_utils.py
+++ pictures1/hills_utils.py
@@ -1,9 +1,7 @@
 from pygame import Surface
-import pygame
 from pygame.draw import *
 from pygame.transform import rotate
 from random import *
-from math import cos, pi, sqrt
 
 
 # one fractal iteration
@@ -47,7 +45,7 @@ def background(w, h):
               (w / 3, h * 2 / 7),
               (w * 3 / 5, h / 7),
               (w, h / 4)])
-    polygon(sf, (168, 168, 168), [(w, h),
+    polygon(sf, (141, 141, 141), [(w, h),
                                   (0, h)] + hills)
     aalines(sf, (0, 0, 0), False, hills)
 
@@ -63,6 +61,9 @@ def background(w, h):
     return sf
 
 
+white = (246, 242, 255)
+
+
 def flower(d):
     sf = Surface((d, d))
     sf.set_colorkey((0, 0, 0))
@@ -74,7 +75,7 @@ def flower(d):
     size = (2 * a, 2 * b)
 
     def draw_petal(x, y):
-        ellipse(sf, (255, 255, 255), (r - a + x, r - b + y) + size)
+        ellipse(sf, white, (r - a + x, r - b + y) + size)
         ellipse(sf, (141, 141, 141), (r - a + x, r - b + y) + size, 1)
 
     draw_petal(0, -b * 3 / 2)
@@ -108,4 +109,63 @@ def bush(d):
     sf.blit(rotate(flower(d / 4), +60), (R * 0.0, R * 0.5))
     sf.blit(rotate(flower(d / 5), +40), (R * 0.4, R * 1.2))
     sf.blit(rotate(flower(d / 5), -90), (R * 1.6, R * 0.9))
+
+    return sf
+
+
+def lama(h):
+    sf = Surface((h, h))
+    sf.set_colorkey((0, 0, 0))
+
+    color = white
+    l = h / 4
+
+    def draw_leg(x, y):
+        leg_w = l * 0.25
+        ellipse(sf, color, (x - l * 0.1, y, leg_w, l * 0.6))
+        ellipse(sf, color, (x - l * 0.1, y + l * 0.5, leg_w, l * 0.6))
+        ellipse(sf, color, (x, y + l, l * 0.3, l * 0.2))
+
+    legs_y = h / 2 + h / 32
+    legs_dy = h / 16
+    legs_x = h / 4
+    legs_dx = h * 3 / 32
+    legs_w = h / 6
+    draw_leg(legs_x + legs_dx, legs_y)
+    draw_leg(legs_x, legs_y - legs_dy)
+    draw_leg(legs_x + legs_w + legs_dx, legs_y)
+    draw_leg(legs_x + legs_w, legs_y - legs_dy)
+
+    body_x = h * 2 / 9
+    body_w = h / 3
+    body_y = h * 2 / 5
+    ellipse(sf, color, (body_x, body_y, body_w, h / 5))
+
+    n = h / 3
+    neck_x = legs_x + legs_w + n * 0.2
+    neck_y = body_y - n * 0.75
+    ellipse(sf, color, (neck_x, neck_y, n * 0.3, n))
+
+    head_w = n / 3
+    head_h = n / 4
+    ellipse(sf, color, (neck_x + n * 0.1, neck_y - n * 0.1, head_w, head_h))
+
+    ear_s = n / 32
+    def draw_ear(x, y):
+        polygon(sf, white, [(x, y),
+                            (x - ear_s * 3, y - ear_s * 2),
+                            (x - ear_s * 5, y - ear_s * 5),
+                            (x, y - ear_s * 2)])
+        
+    draw_ear(neck_x + ear_s * 3, neck_y + ear_s)
+    draw_ear(neck_x + ear_s * 6, neck_y + ear_s)
+
+    eye_x = neck_x + head_w * 3 / 4
+    eye_y = neck_y + head_h / 16
+    circle(sf, (120, 169, 255), (eye_x, eye_y), head_h / 3)
+    circle(sf, (22, 22, 22), (eye_x + head_h /
+           8, eye_y - head_h / 16), head_h / 6)
+    circle(sf, (255, 255, 255), (eye_x + head_h /
+           16, eye_y - head_h / 8), head_h / 10)
+
     return sf
